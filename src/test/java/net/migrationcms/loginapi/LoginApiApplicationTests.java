721d9350b8602ea9f53d4653d2d009beec0c3184
Patrice-H <webmestre64@gmail.com>
2024-05-10 11:29:20 +0200

    initial commit

---
+++ src/test/java/net/migrationcms/loginapi/LoginApiApplicationTests.java
@@ -0,0 +1,13 @@
+package net.migrationcms.loginapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
