052914f5ac87c1e850123733a3059abaf3c504e7
Patrice-H <webmestre64@gmail.com>
2024-05-24 22:17:41 +0200

    Adds controller tests suite

---
+++ src/test/java/net/migrationcms/loginapi/controller/AppUserControllerTests.java
@@ -0,0 +1,12 @@
+package net.migrationcms.loginapi.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class AppUserControllerTests {
+
+    @Test
+	void contextLoads() {
+	}
+}
