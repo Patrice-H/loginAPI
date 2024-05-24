92040a87f143a84e52af5b6701259832084edd0a
Patrice-H <webmestre64@gmail.com>
2024-05-24 22:18:09 +0200

    Adds service tests suite

---
+++ src/test/java/net/migrationcms/loginapi/service/AppUserServiceTests.java
@@ -0,0 +1,12 @@
+package net.migrationcms.loginapi.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class AppUserServiceTests {
+
+    @Test
+	void contextLoads() {
+	}
+}
