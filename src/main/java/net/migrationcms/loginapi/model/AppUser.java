7da0e07dd688448841111c180527acbca9bafb08
Patrice-H <webmestre64@gmail.com>
2024-05-24 22:16:45 +0200

    Udates model

--- src/main/java/net/migrationcms/loginapi/model/AppUser.java
+++ src/main/java/net/migrationcms/loginapi/model/AppUser.java
@@ -6,12 +6,21 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Data
 @Entity
 @Table(name = "app_user")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
 public class AppUser {
 
     @Id
